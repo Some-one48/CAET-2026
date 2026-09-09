efa32a3919549c0bff41c36264278609d3a7e27b
Some_one <hadassafavari48@gmail.com>
2026-09-08 21:00:49 -0300

    navbar c/bootstrap e graficos

--- paginas/graficos.js
+++ paginas/graficos.js
@@ -9,7 +9,7 @@ const info = [
     },
     idad = {
         id: "idade",
-        labels: ['Abaixo de 18', '18 - 25', '26 - 59', 'Acima de 60'],
+        labels: ['Abaixo de 18', '18 a 25', '26 a 59', 'Acima de 60'],
         data: [20,20, 20, 20],
         title: 'Faixa Etária',
     },
@@ -22,15 +22,57 @@ const info = [
     zon = {
         id: "zona",
         labels: ['Zona Urbana', 'Zona Rural'],
-        data: [20,20],
+        data: [173, 32],
         title: 'Zona',
     },
     e = {
         id: "e",
         labels: ['Frequentemente', 'Raramente', 'Nunca'],
         data: [20,20, 20],
-        title: 'Frequência',
+        title: 'Frequência de uso',
     },
+    f = {
+        id: "f",
+        labels: ['Resolução de problemas simples', 'Geração e correção de textos e códigos', 'Pesquisas e auxílio acadêmico', 'Geração de imagens, vídeos, áudios, etc', 'Dúvidas quanto a problemas complexos', 'Não uso'],
+        data: [20,20, 20, 20, 20, 20],
+        title: 'Principal uso',
+    },
+    g = {
+        id: "g",
+        labels: ['Conheço bastante', 'Conheço razoavelmente', 'Conheço pouco', 'Não conheço'],
+        data: [20,20, 20, 20],
+        title: 'Conhecimento quanto aos Efeitos na Sociedade',
+    },
+    h = {
+        id: "h",
+        labels: ['Conheço bastante', 'Conheço razoavelmente', 'Conheço pouco', 'Não conheço'],
+        data: [20,20, 20, 20],
+        title: 'Conhecimento quanto aos Impactos no Meio Ambiente',
+    },
+    i = {
+        id: "i",
+        labels: ['Conheço bastante', 'Conheço razoavelmente', 'Conheço pouco', 'Não conheço'],
+        data: [55, 31, 33, 86],
+        title: 'Sabe o que é um data center',
+    },
+    j = {
+        id: "j",
+        labels: ['Tenho conhecimento', 'Conheço um pouco', 'Não tenho conhecimento'],
+        data: [36, 88, 79],
+        title: 'Conhecimento quanto à coleta de dados',
+    },
+    k = {
+        id: "k",
+        labels: ['Sim', 'Pode dificultar', 'Não'],
+        data: [59, 83, 61],
+        title: 'Acredita que as IAs pode dificultar a busca por emprego',
+    },
+    l = {
+        id: "l",
+        labels: ['Positivamente', 'Negativamente', 'Depende', 'Não sei'],
+        data: [18, 12, 166, 9],
+        title: 'Como a expansão das IAs afetará o futuro',
+    }
 ];
 
 for (item of info){
@@ -41,8 +83,9 @@ for (item of info){
             labels: item.labels,
             datasets: [{
                 data: item.data,
-                backgroundColor:['#FF6384', '#36A2EB', '#ebcd36','#36eb5d','#cd36eb',],
-                borderWidth: 0
+                backgroundColor:['#1b7b3d', '#2dc48d', '#b7ff90','#73d6ee','#1a67a5', '#0d3b66',],
+                borderWidth: 0.4,
+                borderColor: '#0a3323',
             }]
         },
         options: {
@@ -57,6 +100,7 @@ for (item of info){
                 title: {
                     display: true,
                     text: item.title,
+                    color: '#0a3323',
                     font: { size: 20 },
                     padding: { bottom: 5 }
                 }
