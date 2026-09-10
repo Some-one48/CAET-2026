a1af34497956eebec6a3437c3839898e5b2189a4
Some_one <hadassafavari48@gmail.com>
2026-09-10 17:34:13 -0300

    graficos e imagem de entrada

--- paginas/graficos.js
+++ paginas/graficos.js
@@ -3,74 +3,74 @@ let ctx = document.getElementById("genero").getContext("2d");
 const info = [
     gen = {
         id: "genero",
-        labels: ['Masculino', 'Feminino', 'Não-Binário', 'Prefiro não informar', 'Outro'],
-        data: [20,20,20,20,20],
+        labels: ['Masculino', 'Feminino', 'Não-Binário', 'Prefiro não informar'],
+        data: [78, 129, 1, 1],
         title: 'Gênero',
     },
     idad = {
         id: "idade",
         labels: ['Abaixo de 18', '18 a 25', '26 a 59', 'Acima de 60'],
-        data: [20,20, 20, 20],
+        data: [48, 31, 112, 18],
         title: 'Faixa Etária',
     },
     rac = {
         id: "raca",
-        labels: ['Preto', 'Preto', 'Indígena', 'Amarelo', 'Pardo', 'Outro'],
-        data: [20,20,20,20,20,0],
+        labels: ['Preto', 'Branco', 'Indígena', 'Amarelo/Asiático', 'Pardo'],
+        data: [25, 117, 1, 1, 64],
         title: 'Cor/Raça',
     },
     zon = {
         id: "zona",
         labels: ['Zona Urbana', 'Zona Rural'],
-        data: [173, 32],
+        data: [175, 34],
         title: 'Zona',
     },
     e = {
         id: "e",
         labels: ['Frequentemente', 'Raramente', 'Nunca'],
-        data: [20,20, 20],
+        data: [131, 68, 10],
         title: 'Frequência de uso',
     },
     f = {
         id: "f",
         labels: ['Resolução de problemas simples', 'Geração e correção de textos e códigos', 'Pesquisas e auxílio acadêmico', 'Geração de imagens, vídeos, áudios, etc', 'Dúvidas quanto a problemas complexos', 'Não uso'],
-        data: [20,20, 20, 20, 20, 20],
+        data: [44, 30, 63, 62, 3, 7],
         title: 'Principal uso',
     },
     g = {
         id: "g",
         labels: ['Conheço bastante', 'Conheço razoavelmente', 'Conheço pouco', 'Não conheço'],
-        data: [20,20, 20, 20],
+        data: [46, 87, 65, 11],
         title: 'Conhecimento quanto aos Efeitos na Sociedade',
     },
     h = {
         id: "h",
         labels: ['Conheço bastante', 'Conheço razoavelmente', 'Conheço pouco', 'Não conheço'],
-        data: [20,20, 20, 20],
+        data: [48, 25, 26, 110],
         title: 'Conhecimento quanto aos Impactos no Meio Ambiente',
     },
     i = {
         id: "i",
         labels: ['Conheço bastante', 'Conheço razoavelmente', 'Conheço pouco', 'Não conheço'],
-        data: [55, 31, 33, 86],
+        data: [58, 31, 31, 89],
         title: 'Sabe o que é um data center',
     },
     j = {
         id: "j",
         labels: ['Tenho conhecimento', 'Conheço um pouco', 'Não tenho conhecimento'],
-        data: [36, 88, 79],
+        data: [39, 89, 81],
         title: 'Conhecimento quanto à coleta de dados',
     },
     k = {
         id: "k",
         labels: ['Sim', 'Pode dificultar', 'Não'],
-        data: [59, 83, 61],
+        data: [63, 84, 62],
         title: 'Acredita que as IAs pode dificultar a busca por emprego',
     },
     l = {
         id: "l",
         labels: ['Positivamente', 'Negativamente', 'Depende', 'Não sei'],
-        data: [18, 12, 166, 9],
+        data: [18, 11, 170, 10],
         title: 'Como a expansão das IAs afetará o futuro',
     }
 ];
@@ -84,7 +84,7 @@ for (item of info){
             datasets: [{
                 data: item.data,
                 backgroundColor:['#1b7b3d', '#2dc48d', '#b7ff90','#73d6ee','#1a67a5', '#0d3b66',],
-                borderWidth: 0.4,
+                borderWidth: 0.2,
                 borderColor: '#0a3323',
             }]
         },
